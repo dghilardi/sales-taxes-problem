e2f6710c42b61e6854c551351669e234702f8322
Davide Ghilardi <ghilardi.davide@gmail.com>
2019-08-24 12:57:59 +0200

    initialized project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sales Taxes Problem
+
+Proposed solution for Sales Taxes Problem published by xpeppers at the following link https://github.com/xpeppers/sales-taxes-problem
+
+## Prerequisites
+
+ * JDK 1.8+ must be installed and configured on your system. If not installed you can follow the instructions on the following link: https://docs.oracle.com/cd/E19182-01/820-7851/inst_cli_jdk_javahome_t
+ 
+## Build and run
+
+Depending on your operating system open a shell or the command prompt and enter in the project directory
+
+ * Unix systems
+   * build: `./gradlew build`
+   * run: `./gradlew run`
+ * Windows systems
+   * build: `gradlew.bat build`
+   * run: `gradlew.bat run`
---
+++ build.gradle.kts
@@ -0,0 +1,58 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2019 Davide Ghilardi
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin on the JVM.
+    id("org.jetbrains.kotlin.jvm").version("1.3.31")
+
+    // Apply the application plugin to add support for building a CLI application.
+    application
+}
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+}
+
+dependencies {
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    testImplementation("com.nhaarman.mockitokotlin2:mockito-kotlin:2.1.0")
+}
+
+application {
+    // Define the main class for the application
+    mainClassName = "org.ghilardi.salestaxesproblem.AppKt"
+}
