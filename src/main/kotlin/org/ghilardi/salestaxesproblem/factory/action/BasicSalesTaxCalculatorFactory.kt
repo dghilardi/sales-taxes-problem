4ce43e97d0b5d18a2c59d5ec32a97ee23fe41efe
Davide Ghilardi <ghilardi.davide@gmail.com>
2019-08-27 23:51:44 +0200

    create basic sales tax calculator factory

---
+++ src/main/kotlin/org/ghilardi/salestaxesproblem/factory/action/BasicSalesTaxCalculatorFactory.kt
@@ -0,0 +1,43 @@
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2019 Davide Ghilardi
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package org.ghilardi.salestaxesproblem.factory.action
+
+import org.ghilardi.salestaxesproblem.action.impl.FixedRateBasicSalesTaxCalculator
+import org.ghilardi.salestaxesproblem.action.impl.NearestStepRoundingOperation
+import org.ghilardi.salestaxesproblem.action.impl.TaxExemptBasicSalesTaxCalculator
+import org.ghilardi.salestaxesproblem.model.BasketItemType
+import org.ghilardi.salestaxesproblem.model.STPConfiguration
+
+class BasicSalesTaxCalculatorFactory(
+        private val stpConfiguration: STPConfiguration
+) {
+    fun createBasicSalesTaxCalculator(itemType: BasketItemType) = when (itemType) {
+        BasketItemType.FOOD, BasketItemType.MEDICAL_PRODUCT, BasketItemType.BOOK -> TaxExemptBasicSalesTaxCalculator()
+        BasketItemType.OTHER -> FixedRateBasicSalesTaxCalculator(stpConfiguration.basicSalesTaxRate, NearestStepRoundingOperation(stpConfiguration.roundingStep))
+    }
+}
